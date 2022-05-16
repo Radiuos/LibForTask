df806464ad807cba69d7bf7a7650b8a7abd6af42
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-17 01:02:34 +0300

    Update and rename Build.groovy to Execute.groovy

--- vars/Build.groovy
+++
@@ -1,4 +0,0 @@
-def call(def VS_version, def proj_name)
-{
-  bat  """ "C:\\Program Files\\Microsoft Visual Studio\\${VS_version}\\Community\\Common7\\IDE\\devenv.com" ${proj_name}.sln /build "Debug" """
-}
---
+++ vars/Execute.groovy
@@ -0,0 +1,7 @@
+def call(def SolutionName, def task, def Soln_Config_Name, def projname, def project_config, def Output_Filename)
+{
+  def vswhere_path = bat(script: """@"\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe" -property installationPath""", returnStdout: true)
+  def componate_path = '"'+vswhere_path+"\\Common7\\IDE\\devenv.com"+'"'
+  def devenv_path = componate_path.replaceAll("\\r|\\n", "")
+  bat """ ${devenv_path} ${SolutionName} /${task} ${Soln_Config_Name} ${projname} ${project_config} ${Output_Filename}"""
+}
