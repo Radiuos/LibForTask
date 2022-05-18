3d00a2b8553bb17ba3c8f5b8dc94d87fb4568211
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-18 14:42:21 +0300

    Update Execute.groovy

--- vars/Execute.groovy
+++ vars/Execute.groovy
@@ -1,8 +1,5 @@
 def call(def version, def SolutionName, def task, def Soln_Config_Name, def projname = null, def project_config = null, def Output_Filename = null)
 {
-  //projname = projname ?: ''
-  //project_config = project_config ?: ''
-  //Output_Filename = Output_Filename ?: ''
   def vswhere_path = bat(script: """@"\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe" -version ${version} -property installationPath""", returnStdout: true)
   def componate_path = '"'+vswhere_path+"\\Common7\\IDE\\devenv.com"+'"'
   def devenv_path = componate_path.replaceAll("\\r|\\n", "")
