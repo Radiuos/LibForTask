53cca79b29e7b0f70e1d16522b5055d97bd8cca6
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-23 15:16:33 +0300

    Update Transfere.groovy

--- vars/Transfere.groovy
+++ vars/Transfere.groovy
@@ -1,6 +1,6 @@
 def call(def ZipPath, def SolutionName, def Soln_Config_Name)
 {
-  def btime= ${BUILD_TIMESTAMP}
+  def btime = BUILD_TIMESTAMP
   def proj_name = SolutionName.replace(".sln","")
   def foulder = """ ${proj_name}\\bin\\${Soln_Config_Name} """
   //NugetTask\bin\Debug
