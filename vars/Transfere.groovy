1c44ced04307eae47bd09ef24edd4c6449704ee9
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-23 15:30:01 +0300

    Update Transfere.groovy

--- vars/Transfere.groovy
+++ vars/Transfere.groovy
@@ -2,7 +2,8 @@ def call(def ZipPath, def SolutionName, def Soln_Config_Name)
 {
   def btime = BUILD_TIMESTAMP
   def proj_name = SolutionName.replace(".sln","")
-  def foulder = """ ${proj_name}\\bin\\${Soln_Config_Name} """
+  def archive = """${btime}.zip""".replaceAll("\\r|\\n", "")
+  def foulder = """${proj_name}\\bin\\${Soln_Config_Name}"""
   //NugetTask\bin\Debug
-  bat """ ${ZipPath} a -tzip ${btime}.zip ${foulder} """
+  bat """ ${ZipPath} a -tzip ${archive} ${foulder} """
 }
