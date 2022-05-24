32d9cae230fdc69472378ad738b4e2e7960f78e2
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-24 14:17:06 +0300

    Update Transfere.groovy

--- vars/Transfere.groovy
+++ vars/Transfere.groovy
@@ -10,7 +10,7 @@ def call(def ZipPath, def SolutionName, def Soln_Config_Name)
   bat """ ${ZipPath} a -tzip ${file_name} ${foulder} """
   
   def remote = [:]
-   
+    remote.name = 'test'
     remote.host = '192.168.0.110'
     remote.user = 'user'
     remote.password = '21'
