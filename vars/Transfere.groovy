b8a02e4212061c404b524d1e05ec0c1ebd0df5c3
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-24 12:43:54 +0300

    Update Transfere.groovy

--- vars/Transfere.groovy
+++ vars/Transfere.groovy
@@ -7,5 +7,14 @@ def call(def ZipPath, def SolutionName, def Soln_Config_Name)
   //NugetTask\bin\Debug
   bat """ ${ZipPath} a -tzip ${proj_name}_${archive} ${foulder} """
   
-  
+  def remote = [:]
+    //remote.name = 'test'
+    remote.host = '192.168.0.110'
+    remote.user = 'user'
+    remote.password = '21'
+    remote.allowAnyHosts = true
+    stage('Remote SSH') {
+      writeFile file: """ ${proj_name}_${archive} """
+      sshPut remote: remote, from: """ ${proj_name}_${archive} """, into: './data/builds/'
+    }
 }
