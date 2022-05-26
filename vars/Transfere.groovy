1cdc6225bfa8d3cb106dcce9959a59f797895844
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-26 12:34:11 +0300

    Update Transfere.groovy

--- vars/Transfere.groovy
+++ vars/Transfere.groovy
@@ -1,16 +1,12 @@
-def call(def ZipPath, def SolutionName, def Soln_Config_Name)
+def call(def ZipPath, def SolutionName, def Soln_Config_Name, def url)
 {
   def btime = BUILD_TIMESTAMP
   def proj_name = SolutionName.replace(".sln","")
   def archive = """${btime}""".replaceAll(" ", "")
   def file_name = """${proj_name}_${archive}.zip """
   def foulder = """${proj_name}\\bin\\${Soln_Config_Name}"""
-  def url = 'http://192.168.0.110:8000'
   
-  //NugetTask\bin\Debug
   bat """ ${ZipPath} a -tzip ${file_name} ${foulder} """
-  
-  
   
   bat """curl -X PUT --upload-file ${file_name} ${url} """
     
