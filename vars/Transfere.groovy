4ec91943f2236342257f81816c32fe780e311a66
Radiuos <80468351+Radiuos@users.noreply.github.com>
2022-05-30 14:25:14 +0300

    Update Transfere.groovy

--- vars/Transfere.groovy
+++ vars/Transfere.groovy
@@ -6,7 +6,7 @@ def call(def ZipPath, def SolutionName, def Soln_Config_Name, def url)
   def file_name = """${proj_name}_${archive}.zip """
   def foulder = """${proj_name}\\bin\\${Soln_Config_Name}"""
   
-  bat """ ${ZipPath} a -tzip ${file_name} ${foulder} """
+  bat """${ZipPath} a -tzip ${file_name} ${foulder} """
   
   bat """curl -X PUT --upload-file ${file_name} ${url} """
   
